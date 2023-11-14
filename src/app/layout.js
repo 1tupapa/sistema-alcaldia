3c985296b7c016791569620d2e728a197afd4d9b
1tupapa <25042000jeremytorres.asuncion@gmail.com>
2023-11-14 09:11:46 -0600

    Inicializacion del proyecto

---
+++ src/app/layout.js
@@ -0,0 +1,17 @@
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Login | Sistema',
+  description: 'Login para iniciar sesion en el sistema automatizado de la alcaldia de Leon'
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
---
+++ src/app/ui/NavBar.jsx
@@ -0,0 +1,11 @@
+
+
+export const NavBar = () => {
+  return (
+    <>
+        <header>
+            <h1>esto es el header</h1>
+        </header>
+    </>
+  )
+}
